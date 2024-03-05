12426df6619a1bfd0a9b7f480370fc0c6b040dba
Siya <siajabbarova@gmail.com>
2024-03-05 22:02:45 +0400

    create app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          smdlfmsdlkfmsaaaaaaaaaa
         </a>
       </header>
     </div>
